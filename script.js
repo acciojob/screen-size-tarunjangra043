eb8631800e89455f30291e060b8e4d492899ece2
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-02 07:35:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function updateWindowSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    
+    const sizeInfo = document.getElementById('sizeInfo');
+    sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+}
+
+window.addEventListener('resize', updateWindowSize);
+
+updateWindowSize();
